d65b691dc64b750442b0da18c2b7876cd69279a9
Дмитрий <clehofroal@gmail.com>
2024-08-21 05:57:15 +0300

    Проверить ДЗ №3

--- src/Main.java
+++ src/Main.java
@@ -55,7 +55,61 @@ public class Main {
        System.out.println("За 1 месяц или 30 суток машина произвела " + prodIn1Month + " штук бутылок");
 
        System.out.println("  Задача 5.");
+       byte weNeedCans = 120;
+       byte wCansInClass = 2;
+       byte bCansInClass = 4;
+       int forOneClass = wCansInClass + bCansInClass;
+       System.out.println("На ремон школы надо " + weNeedCans + " банок краски.");
+       System.out.println("На один класс надо " + wCansInClass + " банки белой краски и "
+               + bCansInClass + " банки коричневый, суммарно надо " + forOneClass + " банок краски на класс.");
+       int totalClasses = weNeedCans / forOneClass;
+       int wCansForSchool = totalClasses * wCansInClass;
+       int bCansForSchool = totalClasses * bCansInClass;
+       System.out.println("В школе, где " + totalClasses + " классов, нужно " + wCansForSchool +
+               " банок белой краски и " + bCansForSchool + " банок коричневой краски.");
 
+       System.out.println("  Задача 6.");
+       byte bananaWeighs = 80;
+       byte milk100Ml = 105;
+       byte iceCreamBriquette = 100;
+       byte egg = 70;
+       System.out.println("Вес продуктов питания: 1 банан весит " + bananaWeighs +
+               " грамм, 100 мл молока весит " + milk100Ml + " грамм, 1 брекет мороженного-пломбир весит "
+               + iceCreamBriquette + " грамм, 1 яйцо весит " + egg + " грамм.");
+       int theFirstRecipeWeighs = ( 5 * bananaWeighs ) + ( 2 * milk100Ml ) +
+               ( 2 * iceCreamBriquette ) + ( 4 * egg );
+       float theFirstRecipeWeighsKg= theFirstRecipeWeighs / 1000F;
+       System.out.println("Вес одного из рецептов по которому спордсмены готовят себе завтрак равен "
+               + theFirstRecipeWeighs + " грамм или " + theFirstRecipeWeighsKg + " кг.");
 
+       System.out.println("  Задача 7.");
+       short diet250 = 250;
+       short diet500 = 500;
+       byte loseWeightKg = 7;
+       int loseWeightGr = loseWeightKg * 1000;
+       int dayDiet250 = loseWeightGr / diet250;
+       int dayDiet500 = dayDiet250 / 2;
+       int loseWeightOnAverage = (dayDiet250 + dayDiet500) / 2;
+       System.out.println("Если спордсмен будеь терять по " + diet250 + " грамм каждый день, то ему потребуется "
+               + dayDiet250 + " дней, а если будет терять по " + diet500 + " грамм в день, то " + dayDiet500 +
+               " дней что бы сбросить " + loseWeightKg + " кг перед соревнованиями. ");
+       System.out.println("Но при средней потере в веса ему понадобиться " + loseWeightOnAverage + " день.");
+
+       System.out.println("  Задание 8.");
+       int masha = 67760;
+       int denis = 83690;
+       int kristina = 76230;
+       float masha10 = masha * 1.1F;
+       float denis10 = denis * 1.1F;
+       float kristina10 = kristina * 1.1F;
+       float diffSalaryMasha = (masha10 * 12F) - (masha * 12F);
+       float diffSalaryDenis = (denis10 * 12F) - (denis * 12F);
+       float diffSalaryKristina = (kristina10 * 12F) - (kristina * 12F);
+       System.out.println("Маша теперь получает " + masha10 + " рублей. Годовой доход вырос на "
+               + diffSalaryMasha + " рублей");
+       System.out.println("Денис теперь получает " + denis10 + " рублей. Годовой доход вырос на "
+               + diffSalaryDenis + " рублей");
+       System.out.println("Кристина теперь получает " + kristina10 + " рублей. Годовой доход вырос на "
+               + diffSalaryKristina + " рублей");
     }
 }
